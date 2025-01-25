6f3e2b3369eaf32df88e544188a372dcb32b341c
Touafic BOUAB <eclairetoilegentlemans@gmail.com>
2025-01-25 12:53:11 -0500

    Basic pages templating

--- proj/blog/views.py
+++ proj/blog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+
+posts = [
+    {
+        'author': 'hilk',
+        'title': 'Blog Post 1',
+        'content': 'This is the content of the first blog post',
+        'date_posted': 'August 20, 2022'
+    },
+    {
+        'author': 'yacht',
+        'title': 'Blog Post 2',
+        'content': 'This is the content of the second blog post',
+        'date_posted': 'August 21, 2022'
+    },
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
