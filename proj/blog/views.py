baa63fddf018a16131d46d52811e65d9848c929f
Touafic BOUAB <eclairetoilegentlemans@gmail.com>
2025-01-27 09:39:14 -0500

    connection db posts to view page

--- proj/blog/views.py
+++ proj/blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'hilk',
-        'title': 'Blog Post 1',
-        'content': 'This is the content of the first blog post',
-        'date_posted': 'August 20, 2022'
-    },
-    {
-        'author': 'yacht',
-        'title': 'Blog Post 2',
-        'content': 'This is the content of the second blog post',
-        'date_posted': 'August 21, 2022'
-    },
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
