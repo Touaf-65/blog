d7755789d4dfaeb39a9a2d6bcb57aeddd077f9a3
Touafic BOUAB <eclairetoilegentlemans@gmail.com>
2025-01-31 13:26:36 -0500

    add basic user profile section

--- proj/proj/urls.py
+++ proj/proj/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from accounts import views as accounts_views
 
 
@@ -29,3 +31,8 @@ urlpatterns = [
     path('logout/', accounts_views.LogoutView, name='logout'),
     path("blog/", include('blog.urls')),
 ]
+
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
